c61a51e55638e1f6772ee88a079e4345e042bdee
Rajat Khanduja <rajatkhanduja13@gmail.com>
2013-01-09 01:35:14 +0530

    Added README and LICENSE

---
+++ README.md
@@ -0,0 +1,2 @@
+Hacks, here, refer to small programs that might not solve a bigger problem. 
+This would contain programs that I create for fun. These might include programs to try out some new algorithm (but aren't good enough to fit in any other repository of mine) and programs that I try for learning and teaching (yes, I might want to use some of these code samples as examples elsewhere).
--- SpamClassification/bayesClassifier.py
+++ SpamClassification/bayesClassifier.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python
 # Author : Rajat Khanduja
-# Date : 8/1/13
-
 # This program requires spam and ham stored in files named "spam" and "ham"
 # respectively, in the current folder.
 
-# XXX: Call to program
+# Call to program
 # python spamClassifier <spamPrior> <testFile>
+
+#    Copyright (C) 2013 Rajat Khanduja
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 import sys
 
